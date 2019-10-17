e7c29297db115f341a175889007591ee82d71610
Teeeeu <matheus.siqueira32@etec.sp.gov.br>
2019-10-16 21:47:11 -0300

    mudanças e bla bla bla

---
+++ AppNotas/AppNotas/app/src/main/java/br/com/etecia/appnotas/SegundaTela.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segunda_tela_layout);
+    }
+}
