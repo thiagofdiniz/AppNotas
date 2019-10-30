c4ad7265a152635424dc4be4dccb3d18cfd351f6
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-10-30 17:37:10 -0300

    App não abre

--- AppNotas/AppNotas/app/src/main/java/br/com/etecia/appnotas/SegundaTela_Activity.java
+++ AppNotas/AppNotas/app/src/main/java/br/com/etecia/appnotas/SegundaTela_Activity.java
@@ -1,14 +1,33 @@
 package br.com.etecia.appnotas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SegundaTela_Activity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_tela_layout);
+
+        toolbar = findViewById(R.id.idToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
+
