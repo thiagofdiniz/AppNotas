6f7041a6308a7fd30edc787a62c6d3092f5f579a
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-10-30 16:50:58 -0300

    correções

--- AppNotas/AppNotas/app/src/main/java/br/com/etecia/appnotas/SegundaTela_Activity.java
+++ AppNotas/AppNotas/app/src/main/java/br/com/etecia/appnotas/SegundaTela_Activity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SegundaTela extends AppCompatActivity {
+public class SegundaTela_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
